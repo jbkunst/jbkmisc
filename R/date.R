8334ddeaea08a27213e984db170d38e4bbbd0bd8
Joshua <jbkunst@gmail.com>
2017-02-13 12:49:24 -0300

    adding ym_div

--- R/date.R
+++ R/date.R
@@ -23,3 +23,29 @@ ym_diff <- function(ym = c(200902, 201912), ym2 = c(200901, 201712)) {
 
 }
 
+#' Year month to semester, quarter, trimester
+#' @param ym ym
+#' @param d division parameter. d = 2 for semesters, d = 4 for quarters, etc
+#' @export
+ym_div <- function(ym = format(ymd(20170101) + months(0:11), "%Y%m"), d = 4) {
+
+  stopifnot(d %in% c(2,3,4,6))
+
+  cuts <- (seq(1, 12/d) - 1) * d + 1
+
+  ymd <- ym_to_date(ym)
+
+  ms <- lubridate::month(ymd)
+
+  new_ms <- cut(ms, breaks = unique(c(cuts, 12)),
+                labels = unique(cuts), include.lowest = TRUE, right = FALSE)
+  new_ms <- as.character(new_ms)
+  new_ms <- as.numeric(new_ms)
+
+  lubridate::month(ymd) <- new_ms
+
+  format(ymd, "%Y%m")
+
+}
+
+
